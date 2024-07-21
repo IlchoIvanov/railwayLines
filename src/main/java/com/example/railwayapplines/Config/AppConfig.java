4936d489646effd13581e80498c664e5ac7e7981
ilcho <ilcho1011@abv.bg>
2024-07-21 14:54:33 +0300

    database init

---
+++ src/main/java/com/example/railwayapplines/Config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.example.railwayapplines.Config;
+
+import com.example.railwayapplines.Repository.RailwayLineRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource,
+                                                       RailwayLineRepository railwayLineRepository,
+                                                       ResourceLoader resourceLoader) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+
+        if (railwayLineRepository.count() == 0) {
+            ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+            populator.addScript(resourceLoader.getResource("classpath:data.sql"));
+            initializer.setDatabasePopulator(populator);
+        }
+
+        return initializer;
+    }
+}
